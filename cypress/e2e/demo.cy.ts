ed29d6aa4fe04f1b7e6fb0db05e4f34d6bcfe8e5
Ruslan Maistrenko <rmaistrenko@verenia.com>
2023-02-16 15:29:00 +0200

    q1

--- cypress/e2e/demo.cy.ts
+++ cypress/e2e/demo.cy.ts
@@ -1,8 +1,7 @@
 describe("template spec", () => {
-  it("passes", () => {
+  beforeEach(() => {
     cy.visit("https://example.cypress.io");
   });
-
   it("Does not do much!", () => {
     expect(true).to.equal(true);
   });
